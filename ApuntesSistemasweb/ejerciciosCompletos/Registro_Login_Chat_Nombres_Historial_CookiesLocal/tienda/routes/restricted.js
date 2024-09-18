9d13973c12e6fbb1acbd421406049e2f9a3d49a8
Miguel <miguel.merinoplaza@gmail.com>
2024-09-19 01:28:05 +0200

    cambios

--- ApuntesSistemasweb/ejerciciosCompletos/Registro_Login_Chat_Nombres_Historial_CookiesLocal/tienda/routes/restricted.js
+++ ApuntesSistemasweb/ejerciciosCompletos/Registro_Login_Chat_Nombres_Historial_CookiesLocal/tienda/routes/restricted.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Armazón', user: req.session.user });
+  res.render('restricted', { title: 'Restricted', user: req.session.user});
 });
 
 module.exports = router;
